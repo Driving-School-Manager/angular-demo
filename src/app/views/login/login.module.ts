79adb03b14e5884ec31d1cabd1ae3ee17a3759ce
Przemysław Frąckowiak-Szymański <7953131+dehydrogenaza@users.noreply.github.com>
2023-03-27 16:26:33 +0200

    demo

--- src/app/views/login/login.module.ts
+++ src/app/views/login/login.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {LoginComponent} from "./login.component";
 import {RouterModule} from "@angular/router";
-import {HomeComponent} from "../home/home.component";
 
 
 
