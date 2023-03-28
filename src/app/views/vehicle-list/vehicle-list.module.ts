c550b7da36393f50b68b1814ad2a54ae4bccc0cd
Jakub Piecuch <kuba.piecuch.kp@gmail.com>
2023-03-28 15:05:26 +0200

    modified template for admin and added it to vehicle-list

--- src/app/views/vehicle-list/vehicle-list.module.ts
+++ src/app/views/vehicle-list/vehicle-list.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { VehicleListComponent } from './vehicle-list.component';
+import { AdminPanelModule } from '../admin-panel/admin-panel.module';
 
 @NgModule({
   declarations: [VehicleListComponent],
   imports: [
     CommonModule,
+    AdminPanelModule,
     RouterModule.forChild([
       {
         path: '',
